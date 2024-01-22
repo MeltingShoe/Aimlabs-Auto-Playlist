75efda1f6a375e92843d4bf1c45a0b6fe70cfe87
Ryan Harper <meltingshoe@gmail.com>
2024-01-21 16:35:49 -0800

    Made an SQL expression that significantly speeds up reading the db by filtering out unneeded results. You know like the literal only reason SQL exists?

--- AAT/readDB.py
+++ AAT/readDB.py
@@ -1,10 +1,39 @@
 import sqlite3
 from utils import openYAML
 from logger import log, logLevel, debug, info, warning, error, critical
+import os
 
 
 class readAimlabsDB:
+    @log
     def __init__(self):
-        config = openYAML('userConfig')
+        config = openYAML('userConfig.yaml')
         self.dbPath = os.path.abspath(os.path.join(os.getenv(
             "APPDATA"), os.pardir, config['dbPath']))
+
+    @log
+    def __call__(self):
+        return self.readDB()
+
+    @log
+    def readDB(self):
+        testID = 1700
+        con = sqlite3.connect(self.dbPath)
+        cur = con.cursor()
+        res = cur.execute(
+            "SELECT taskId, klutchId, createDate, taskName, score, performance FROM TaskData WHERE taskId > 1500 AND taskName LIKE '%meltingshoe%' OR taskId > 1500 AND taskName LIKE '%hartrean%'")
+        out = res.fetchall()
+        warning(out)
+        self.data = out
+        return out
+
+
+def main():
+    db = readAimlabsDB()
+
+    for item in db():
+        warning(item)
+
+
+if __name__ == '__main__':
+    main()
